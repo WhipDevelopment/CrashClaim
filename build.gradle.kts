0952621655e8580dbd44e66641931a737c5913e0
Brandon Nifong <brandon@tendency.me>
2022-08-21 01:07:59 -0500

    chore(plugin): Add 1.19.1 and 1.19.2 to the CompatabilityManager chore(deps): Bump Adventure to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
 
     // Adventure
     implementation("net.kyori:adventure-api:4.11.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.1.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.1.1")
     implementation("net.kyori:adventure-text-minimessage:4.11.0")
 
     // Other

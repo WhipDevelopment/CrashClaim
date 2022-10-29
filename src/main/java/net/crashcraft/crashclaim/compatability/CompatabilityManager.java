2c7688d9fcda43493f90d843d2445ba283d40d14
Brandon <brandon@tendency.me>
2022-10-28 21:56:32 -0500

    chore(plugin): Add 1.19.1 and 1.19.2 to the CompatabilityManager (#19)
    chore(deps): Bump Adventure to latest version

--- src/main/java/net/crashcraft/crashclaim/compatability/CompatabilityManager.java
+++ src/main/java/net/crashcraft/crashclaim/compatability/CompatabilityManager.java
@@ -35,7 +35,9 @@ public class CompatabilityManager {
             Wrapper1_18_0.class,
             Wrapper1_18_1.class,
             Wrapper1_18_2.class,
-            Wrapper1_19_0.class
+            Wrapper1_19_0.class,
+            Wrapper1_19_1.class,
+            Wrapper1_19_2.class
     );
 
     public CompatabilityManager(ProtocolManager manager){
---
+++ src/main/java/net/crashcraft/crashclaim/compatability/versions/Wrapper1_19_1.java
@@ -0,0 +1,5 @@
+package net.crashcraft.crashclaim.compatability.versions;
+
+public class Wrapper1_19_1 extends Wrapper1_19_0 {
+    // No changes
+}

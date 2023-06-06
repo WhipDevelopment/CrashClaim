ee1548162edbe81449bea9fb18c23bc27db2738e
DaJokni <56400974+DaJokni@users.noreply.github.com>
2023-06-07 00:31:40 +0300

    Add support for 1.19.4

--- src/main/java/net/crashcraft/crashclaim/compatability/CompatabilityManager.java
+++ src/main/java/net/crashcraft/crashclaim/compatability/CompatabilityManager.java
@@ -38,7 +38,8 @@ public class CompatabilityManager {
             Wrapper1_19_0.class,
             Wrapper1_19_1.class,
             Wrapper1_19_2.class,
-            Wrapper1_19_3.class
+            Wrapper1_19_3.class,
+            Wrapper1_19_4.class
     );
 
     public CompatabilityManager(ProtocolManager manager){
